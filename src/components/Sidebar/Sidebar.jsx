f4b32d949047189516d3e86d0c4b6bbd21b7881c
Ronak Agarwal <agarwalronak58@gmail.com>
2023-10-15 01:06:58 -0400

    Finished UI for sidebar:

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,69 +1,113 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { MdClose, MdMenu, MdHome, MdEqualizer } from "react-icons/md";
-import MenuItem from "./MenuItem";
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import styles from "./Sidebar.module.css";
+import { FaFire, FaBars } from "react-icons/fa";
+import { IoIosStats } from "react-icons/io";
 
-const gradientColors = ["#0A0F0B", "#228B22"];
+const transitionTime = "0.2s";
 
-const Sidebar = () => {
-  const navigate = useNavigate();
+const SideBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const sidebarRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-  const handleMouseEnter = (index) => {
-    setHoveredIndex(index);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
   };
 
-  const handleMouseLeave = () => {
-    setHoveredIndex(null);
+  const handleScroll = (event) => {
+    const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
+    setScrollPosition(scrollTop / (scrollHeight - clientHeight));
+  };
+
+  useEffect(() => {
+    if (sidebarRef.current) {
+      sidebarRef.current.style.maxHeight = isOpen
+        ? `${sidebarRef.current.scrollHeight}px`
+        : "55px";
+    }
+  }, [isOpen]);
+
+  const renderMenuItems = () => {
+    return menuItems.map((item, index) => (
+      <React.Fragment key={index}>
+        {index === 0 && <Divider isOpen={isOpen} />}
+        <Link
+          to={item.to}
+          style={{
+            transition: `all ${transitionTime} ease-in-out ${
+              isOpen ? 0.2 * index : (menuItems.length + 1 - index) * 0.05
+            }s`, // Updated transition time
+            transform: isOpen ? "translateX(0)" : "translateX(-120%)", // New transform for expanding effect
+          }}
+        >
+          <SideBarItem icon={item.icon} text={item.text} />
+        </Link>
+      </React.Fragment>
+    ));
   };
 
   const menuItems = [
-    { icon: "home", text: "Home", to: "/" },
-    { icon: "equalizer", text: "Stats", to: "Stats" },
-    // ... add other items as needed
+    {
+      to: "/",
+      icon: <FaFire size="28" />,
+      text: "Home",
+    },
+    {
+      to: "/stats",
+      icon: <IoIosStats size="20" />,
+      text: "Stats",
+    },
   ];
 
   return (
-    <div className={styles.sidebarContainer}>
+    <div className={styles.container}>
       <div
-        className={styles.sidebar}
-        style={{
-          transform: `translateX(${isOpen ? "0px" : "-250px"})`,
-          transition: "transform 300ms",
-        }} // Added transition here
+        className={`${styles.sidebar} ${styles.sidebarOpen}`}
+        ref={sidebarRef}
+        onScroll={handleScroll}
       >
-        <button
-          className={styles.closeButton}
-          onClick={() => setIsOpen(!isOpen)}
+        <div
+          className={`relative h-14 w-14 flex items-center justify-center bg-gray-900 rounded-full`}
+          onClick={toggleSidebar}
+          style={{ transition: `transform 0.3s` }}
         >
-          <MdClose size={24} color="#FFF" />
-        </button>
-        {menuItems.map((item, index) => (
-          <MenuItem
-            key={index}
-            icon={item.icon}
-            text={item.text}
-            to={item.to}
-            isHovered={hoveredIndex === index}
-            gradientColors={gradientColors}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={handleMouseLeave}
-          />
-        ))}
-      </div>
-      <div className={styles.menuButton}>
-        <button onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? (
-            <MdClose size={24} color="#FFF" />
-          ) : (
-            <MdMenu size={24} color="#FFF" />
-          )}
-        </button>
+          <div className={`${styles.sidebarIcon}`}>
+            <SideBarItem
+              icon={<FaBars size="20" />}
+              text={isOpen ? "Close Sidebar" : "Open Sidebar"}
+              isOpen={isOpen}
+            />
+          </div>
+        </div>
+
+        {renderMenuItems()}
       </div>
     </div>
   );
 };
 
-export default Sidebar;
+const SideBarItem = ({ icon, text = "tooltip 💡", isOpen }) => (
+  <div className={`${styles.sidebarIcon} relative`}>
+    <div>{icon}</div>
+    <span
+      className={`${styles.sidebarTooltip} group-hover:scale-100`}
+      style={{ transition: `transform ${transitionTime} ease-in-out` }}
+    >
+      {text}
+    </span>
+  </div>
+);
+
+const Divider = ({ isOpen, transitionDelay }) => (
+  <hr
+    className={`${styles.sidebarHr}`}
+    style={{
+      transition: `all ${transitionTime} ease-in-out ${transitionDelay}s`,
+      transform: isOpen ? "translateX(0)" : "translateX(-120%)",
+      opacity: isOpen ? 1 : 0,
+    }}
+  />
+);
+
+export default SideBar;
