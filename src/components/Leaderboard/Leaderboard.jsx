6b9588e92e5e84019eda6451a90caf809bfda339
sohanm04 <84931341+sohanm04@users.noreply.github.com>
2023-10-15 04:13:00 -0400

    Changed leaderboard a little bit

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -124,7 +124,7 @@ const Leaderboard = () => {
             <p className={styles.modalText}>Items Trashed: {selectedBubbleInfo.itemsTrashed}</p>
             <p className={styles.modalText}>Items Composted: {selectedBubbleInfo.itemsComposted}</p>
             <div className={styles.mapContainer}>
-              <MapContainer center={initialCoordinates} zoom={15} style={{ width: '100%', height: '100%' }} attributionControl={false}>
+            <MapContainer center={initialCoordinates} zoom={15} style={{ width: '100%', height: '100%' }} attributionControl={false}>
                   <TileLayer
                     url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
                     maxZoom={19}
